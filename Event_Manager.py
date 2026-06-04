c1336e7c7cf25640b4f2ee5a1939faf966f72be0
vamshi01234 <tufranvamshi@gmail.com>
2026-06-04 18:54:18 -0500

    Added Event feature

--- Event_Manager.py
+++ Event_Manager.py
@@ -5,7 +5,8 @@ while True:
     print("\n--- Personal Memory Manager ---")
     print("1. Add Person")
     print("2. View People")
-    print("3. Exit")
+    print("3. Add Event")
+    print("4. Exit")
 
     choice = input("Choose an option: ")
 
@@ -32,6 +33,36 @@ while True:
             print("\nName:", person["name"])
 
     elif choice == "3":
+
+        name = input("Enter person's name: ")
+
+        found = False
+
+        for person in people:
+
+            if person["name"] == name:
+
+                found = True
+
+                print("Person found!")
+
+                event_type = input("Enter event type (Birthday, Anniversary, etc): ")
+                event_date = input("Enter event date (MM-DD): ")
+
+                event = {
+                    "type": event_type,
+                    "date": event_date
+                }
+
+                person["events"].append(event)
+
+                print("Event added!")
+    
+                break
+
+        if not found:
+            print("Person not found ❌")
+    elif choice == "4":
         break
 
     else:
